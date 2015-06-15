81bb80ae5600daef406a63789100bab6d137dfb0
Mateusz Lorencki <mateuszlor@wp.pl>
2015-06-16 01:12:38 +0200

    Project class fixed

--- src/main/java/pl/lodz/p/it/java/model/Projects.java
+++ src/main/java/pl/lodz/p/it/java/model/Projects.java
@@ -32,149 +32,106 @@ import javax.xml.bind.annotation.XmlRootElement;
 @TableGenerator(name = "ProjectsIdGen", table = "generator", pkColumnName = "class_name", valueColumnName = "id_range", pkColumnValue = "Projects")
 @XmlRootElement
 @NamedQueries({
-		@NamedQuery(name = "Projects.findAll", query = "SELECT p FROM Projects p"),
-		@NamedQuery(name = "Projects.findByDescription", query = "SELECT p FROM Projects p WHERE p.description = :description"),
-		@NamedQuery(name = "Projects.findById", query = "SELECT p FROM Projects p WHERE p.id = :id") })
+    @NamedQuery(name = "Projects.findAll", query = "SELECT p FROM Projects p"),
+    @NamedQuery(name = "Projects.findByDescription", query = "SELECT p FROM Projects p WHERE p.description = :description"),
+    @NamedQuery(name = "Projects.findById", query = "SELECT p FROM Projects p WHERE p.id = :id")})
 public class Projects implements Serializable {
-	private static final long serialVersionUID = 1L;
-	@Size(max = 2147483647)
-	@Column(name = "Description")
-	private String description;
-	@Id
-	@Basic(optional = false)
-	@NotNull
-	@GeneratedValue(strategy = GenerationType.TABLE, generator = "ProjectsIdGen")
-	@Column(name = "Id")
-	private Integer id;
 
-	// bi-directional many-to-one association to Projectcapacitor
-	@OneToMany(mappedBy = "projectBean")
-	private List<Projectcapacitor> projectcapacitors;
+    private static final long serialVersionUID = 1L;
+    @Size(max = 2147483647)
+    @Column(name = "Description")
+    private String description;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "ProjectsIdGen")
+    @Column(name = "Id")
+    private Integer id;
 
-	// bi-directional many-to-one association to Projectuc
-	@OneToMany(mappedBy = "projectBean")
-	private List<Projectuc> projectucs;
+    // bi-directional many-to-one association to Projectcapacitor
+    @OneToMany(mappedBy = "projectBean")
+    private List<Capacitors> projectcapacitors;
 
-	// bi-directional many-to-one association to Projectresistor
-	@OneToMany(mappedBy = "projectBean")
-	private List<Projectresistor> projectresistors;
+    // bi-directional many-to-one association to Projectuc
+    @OneToMany(mappedBy = "projectBean")
+    private List<UCs> projectucs;
 
-	public Projects() {
-	}
+    // bi-directional many-to-one association to Projectresistor
+    @OneToMany(mappedBy = "projectBean")
+    private List<Resistors> projectresistors;
 
-	public Projects(Integer id) {
-		this.id = id;
-	}
+    public Projects() {
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public Projects(Integer id) {
+        this.id = id;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public Integer getId() {
-		return id;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public List<Projectcapacitor> getProjectcapacitors() {
-		return this.projectcapacitors;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public void setProjectcapacitors(List<Projectcapacitor> projectcapacitors) {
-		this.projectcapacitors = projectcapacitors;
-	}
+    public List<Capacitors> getProjectcapacitors() {
+        return this.projectcapacitors;
+    }
 
-	public Projectcapacitor addProjectcapacitor(
-			Projectcapacitor projectcapacitor) {
-		getProjectcapacitors().add(projectcapacitor);
-		projectcapacitor.setProjectBean(this);
+    public void setProjectcapacitors(List<Capacitors> projectcapacitors) {
+        this.projectcapacitors = projectcapacitors;
+    }
 
-		return projectcapacitor;
-	}
+    public List<UCs> getProjectucs() {
+        return this.projectucs;
+    }
 
-	public Projectcapacitor removeProjectcapacitor(
-			Projectcapacitor projectcapacitor) {
-		getProjectcapacitors().remove(projectcapacitor);
-		projectcapacitor.setProjectBean(null);
+    public void setProjectucs(List<UCs> projectucs) {
+        this.projectucs = projectucs;
+    }
 
-		return projectcapacitor;
-	}
+    public List<Resistors> getProjectresistors() {
+        return this.projectresistors;
+    }
 
-	public List<Projectuc> getProjectucs() {
-		return this.projectucs;
-	}
+    public void setProjectresistors(List<Resistors> projectresistors) {
+        this.projectresistors = projectresistors;
+    }
 
-	public void setProjectucs(List<Projectuc> projectucs) {
-		this.projectucs = projectucs;
-	}
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
 
-	public Projectuc addProjectuc(Projectuc projectuc) {
-		getProjectucs().add(projectuc);
-		projectuc.setProjectBean(this);
-
-		return projectuc;
-	}
-
-	public Projectuc removeProjectuc(Projectuc projectuc) {
-		getProjectucs().remove(projectuc);
-		projectuc.setProjectBean(null);
-
-		return projectuc;
-	}
-
-	public List<Projectresistor> getProjectresistors() {
-		return this.projectresistors;
-	}
-
-	public void setProjectresistors(List<Projectresistor> projectresistors) {
-		this.projectresistors = projectresistors;
-	}
-
-	public Projectresistor addProjectresistor(Projectresistor projectresistor) {
-		getProjectresistors().add(projectresistor);
-		projectresistor.setProjectBean(this);
-
-		return projectresistor;
-	}
-
-	public Projectresistor removeProjectresistor(Projectresistor projectresistor) {
-		getProjectresistors().remove(projectresistor);
-		projectresistor.setProjectBean(null);
-
-		return projectresistor;
-	}
-
-	@Override
-	public int hashCode() {
-		int hash = 0;
-		hash += (id != null ? id.hashCode() : 0);
-		return hash;
-	}
-
-	@Override
-	public boolean equals(Object object) {
+    @Override
+    public boolean equals(Object object) {
 		// TODO: Warning - this method won't work in the case the id fields are
-		// not set
-		if (!(object instanceof Projects)) {
-			return false;
-		}
-		Projects other = (Projects) object;
-		if ((this.id == null && other.id != null)
-				|| (this.id != null && !this.id.equals(other.id))) {
-			return false;
-		}
-		return true;
-	}
+        // not set
+        if (!(object instanceof Projects)) {
+            return false;
+        }
+        Projects other = (Projects) object;
+        if ((this.id == null && other.id != null)
+                || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
 
-	@Override
-	public String toString() {
-		return "pl.lodz.p.it.java.model.Projects[ id=" + id + " ]";
-	}
+    @Override
+    public String toString() {
+        return "pl.lodz.p.it.java.model.Projects[ id=" + id + " ]";
+    }
 
 }
